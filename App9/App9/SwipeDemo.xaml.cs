887b07db2825b7423ea20feda4075f8681943d26
V3lasquez <noe.vc1782@gmail.com>
2023-11-18 11:16:12 -0500

    gesture

---
+++ App9/App9/SwipeDemo.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+
+namespace App9
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SwipeDemo : ContentPage
+    {
+        public SwipeDemo()
+        {
+            InitializeComponent();
+        }
+        void OnSwiped(object sender, SwipedEventArgs e)
+        {
+            string direction = e.Direction.ToString();
+            if (direction == "Right")
+                ((BoxView)sender).Background = new SolidColorBrush(Color.Red);
+            if (direction == "Left")
+                ((BoxView)sender).Background = new SolidColorBrush(Color.Red);
+            if (direction == "Up")
+                ((BoxView)sender).Background = new SolidColorBrush(Color.Red);
+            if (direction == "Down")
+                ((BoxView)sender).Background = new SolidColorBrush(Color.Red);
+            _label.Text = $"You swiped: {e.Direction.ToString()}";
+        }
+    }
+}
